c65dfbf1d92e4b6cb8afdf6afe51f33119c0cffa
anushah-200 <77103799+anushah-200@users.noreply.github.com>
2021-04-26 19:26:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
+
 var bananaimg,obstacleimg,background,score,monkey,monkeyrun,stone,
 bg,ground,score,bgimg;
-
 function preload(){monkeyrun=loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png")
                    
 bananaimg=loadImage("banana.png")
@@ -11,13 +11,13 @@ bgimg=loadImage("jungle.jpg")}
 
 
 function setup() {
-  createCanvas(height, width);
-  bg=createSprite(height-200,width-200,200,200)
+  createCanvas(400, 400);
+  bg=createSprite(200,200,200,200)
   bg.addImage(bgimg)
- monkey = createSprite(height-350,width-200,20,50);
+ monkey = createSprite(50,200,20,50);
 monkey.addAnimation("monkeyrun",monkeyrun);
   monkey.scale=0.12
- ground=createSprite(height-100,width-20,800,20)
+ ground=createSprite(300,380,800,20)
   ground.visible=false  
   bananaGroup=new Group()
     stoneGroup=new Group()
@@ -26,7 +26,6 @@ monkey.addAnimation("monkeyrun",monkeyrun);
   bg.velocityX=-5
    score=0
 }
-
 function draw() {
   background(220);
    if(keyDown("space")&&monkey.collide(ground)) {
@@ -53,20 +52,18 @@ if(bg.x<0){
  }
  if(monkey.isTouching(stoneGroup)) {
    monkey.scale=0.12}
- camera.position.x=bananaGroup.x
-  camera.position.y=bananaGroup.y
-  
+
+
 
  drawSprites()
-   text("score "+ score, height-50,width-300);
+   text("score "+ score, 350, 100);
   spawnObstacles() 
   spawnbananas()
-
 }
 
 function spawnbananas(){
   if (frameCount % 80 === 0) {
-     banana = createSprite(height,width-80,40,10);
+     banana = createSprite(400,320,40,10);
     banana.y = Math.round(random(220,280))
     banana.addImage(bananaimg);
     banana.scale = 0.1
@@ -82,7 +79,7 @@ function spawnbananas(){
 }
 function spawnObstacles() {
   if(frameCount % 300 === 0) {
-    stone = createSprite(height,width-35,10,40);
+    stone = createSprite(400,365,10,40);
     stone.velocityX = - 5
     stone.addImage(stoneimg);           
     stone.scale = 0.1;
@@ -91,10 +88,6 @@ function spawnObstacles() {
     stoneGroup.depth=bg.depth+1
   }
 }
-
-
-
-
 
 
 
